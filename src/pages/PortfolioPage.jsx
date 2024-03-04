9ad9f51e78a691bd67abeadaf463c9401ec69248
Jared Woodcock <jared.woodcock@yahoo.com>
2024-03-04 00:36:59 -0600

    Moved all pictures to public folder

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -5,42 +5,42 @@ export default function PortfolioPage() {
         {
             title: 'Worth The Watch',
             description: 'An app where you can search for movies, leave reviews, and see other user reviews',
-            imageUrl: '../src/assets/worth-the-watch-ss.png',
+            imageUrl: '../public/worth-the-watch-ss.png',
             demoLink: 'https://worth-the-watch-4fed4e43ed4e.herokuapp.com/',
             githubLink: 'https://github.com/ExecutorKarthan/Worth-the-Watch.git'
         },
         {
             title: 'Note Taker',
             description: 'An app for taking notes',
-            imageUrl: '../src/assets/note-taker-ss.png',
+            imageUrl: '../public/note-taker-ss.png',
             demoLink: 'https://note-taker-jwbootcamp-9c71d447c89c.herokuapp.com/',
             githubLink: 'https://github.com/JaredWoodcock/note-taker.git'
         },
         {
             title: 'Weather Dashboard',
             description: 'An app to search for the weather in a specific city, with 5 day forecast',
-            imageUrl: '../src/assets/Weather-Dashboard-ss.png',
+            imageUrl: '../public/Weather-Dashboard-ss.png',
             demoLink: 'https://jaredwoodcock.github.io/Weather-Dashboard/',
             githubLink: 'https://github.com/JaredWoodcock/Weather-Dashboard.git'
         },
         {
             title: 'Work Day Scheduler',
             description: 'A scheduling app with responsive time slots',
-            imageUrl: '../src/assets/Work-Day-Scheduler-ss.png',
+            imageUrl: '../public/Work-Day-Scheduler-ss.png',
             demoLink: 'https://jaredwoodcock.github.io/Work-Day-Scheduler/',
             githubLink: 'https://github.com/JaredWoodcock/Work-Day-Scheduler.git'
         },
         {
             title: 'Coding Quiz',
             description: 'A coding quiz to test your knowledge',
-            imageUrl: '../src/assets/Coding-Quiz-ss.png',
+            imageUrl: '../public/Coding-Quiz-ss.png',
             demoLink: 'https://jaredwoodcock.github.io/Coding-Quiz/index.html',
             githubLink: 'https://github.com/JaredWoodcock/Coding-Quiz.git'
         },
         {
             title: 'Password Generator',
             description: 'An app that generates a random password based off of the answered prompts from the user',
-            imageUrl: '../src/assets/password-generator-ss.png',
+            imageUrl: '../public/password-generator-ss.png',
             demoLink: 'https://jaredwoodcock.github.io/password-generator/',
             githubLink: 'https://github.com/JaredWoodcock/password-generator.git'
         },
