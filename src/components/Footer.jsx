862c7cfa14f79206301e87df33a8d384e5d8ca2e
Jared Woodcock <jared.woodcock@yahoo.com>
2024-03-04 00:19:44 -0600

    Completed footer and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
+
 export default function Footer() {
     return (
         <footer className="py-4">
             <div className="text-center">
-                <a className="list-inline-item" href="https://github.com/JaredWoodcock" target="_blank" rel="noopener noreferrer">GitHub</a>
-
+                <a className="list-inline-item" href="https://github.com/JaredWoodcock" target="_blank" rel="noopener noreferrer">
+                    GitHub <FontAwesomeIcon icon={faGithub} />
+                </a>
                 <div className="list-inline-item">|</div>
 
-                <a className="list-inline-item" href="" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a className="list-inline-item" href="https://www.linkedin.com/in/jared-woodcock/" target="_blank" rel="noopener noreferrer">
+                    LinkedIn <FontAwesomeIcon icon={faLinkedin} />
+                </a>
                 
                 <div className="list-inline-item">|</div>
 
-                <a className="list-inline-item" href="" target="_blank" rel="noopener noreferrer">Another Link</a>
+                <a className="list-inline-item" href="https://stackoverflow.com/users/23524666/jaredw" target="_blank" rel="noopener noreferrer">
+                    Stack Overflow <FontAwesomeIcon icon={faStackOverflow} />
+                </a>
             </div>
         </footer>
     );
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -3,8 +3,12 @@ import ContactForm from '../components/ContactForm';
 export default function ContactPage() {
     return (
         <div className='m-5'>
-            <h2>Contact Me</h2>
+            <h2>Leave Me a Message</h2>
             <ContactForm />
+            <div className='text-center'>
+                <p>I can also be contacted at:</p>
+                <a style={{ color: '#0077b6'}} href="mailto:jared.woodcock@yahoo.com">jared.woodcock@yahoo.com</a>
+            </div>
         </div>
     );
 };
