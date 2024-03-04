fe30501591e87eacb57f075cb214bdef132d932a
Jared Woodcock <jared.woodcock@yahoo.com>
2024-03-03 20:24:54 -0600

    Completed resume page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -2,14 +2,28 @@ export default function ResumePage() {
     return (
         <div className="m-5">
             <h2 className="pb-3">Resume</h2>
-            <a href="" download>Click here to download my resume</a>
+            <a style={{ color: '#0077b6' }} href="../src/assets/JW Resume 2024.pdf" download>Download my resume here</a>
 
-            <div>
-                <h4>Proficiencies</h4>
+            <div className="mt-5">
+                <h4>Front-End Proficiencies</h4>
                 <ul>
-                    <li>JavaScript</li>
                     <li>HTML</li>
                     <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>JQuery</li>
+                    <li>Responsive Design</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                </ul>
+                <h4>Back-End Proficiencies</h4>
+                <ul>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL, Sequelize</li>
+                    <li>MongoDB, Mongoose</li>
+                    <li>REST</li>
+                    <li>GraphQL</li>
                 </ul>
             </div>
         </div>
